403554475bf0d575985cbb639b2326e94d02fa86
pitowalosian <sian_obregon@dlsu.edu.ph>
2026-08-05 16:18:42 +0800

    average asm time complete

--- README.md
+++ README.md
@@ -12,12 +12,12 @@
 - Output in GitHub (make sure that I can access your Github):
 
 ### Average Execution Time 
- Y  | Time (s)
---- | --- 
-10 | 
-100 |
-1000 |
-9000 |
+ Y  | Average Time (ms) | Min | Max
+--- | --- | --- | ---
+10 | 0.00019 |	0.0001 | 0.0005		
+100 | 0.00066 | 0.0004 | 0.0013  	
+1000 | 0.00544 | 0.0038 | 0.0208
+9000 | 0.03131 | 0.0177 | 0.0476
 
 ### Short Analysis of the Performance
 
--- x86-to-C interface/x86-to-C interface/clbyarch-mp2.c
+++ x86-to-C interface/x86-to-C interface/clbyarch-mp2.c
@@ -4,64 +4,88 @@
 #include <Windows.h>
 
 #define MAX_CARS 9000
+#define MAX_TEST 30
 
 extern int lf_to_int(double lf);
 extern double calc_accel(double in_v, double fin_v, double time);
 
+/* void save_time(double calc_accel_ms[], int test_number) {
+	FILE* file = fopen("9000_time.txt", "a"); // Open in append mode
+
+	if (file == NULL) {
+		printf("Error opening file for writing.\n");
+		return;
+	}
+
+	fprintf(file, "%.6lf\n", calc_accel_ms[test_number]);
+
+	fclose(file);
+}
+*/
+
 int main() {
-	double y = 0.0;
-	double in_v[MAX_CARS] = { 0.0 };
-	double fin_v[MAX_CARS] = { 0.0 };
-	double time_s[MAX_CARS] = { 0.0 };
+	srand((unsigned int)time(NULL)); // Seed the random number generator
 
-	srand(42); // Seed the random number generator
+	//int k = 0;
+	double calc_accel_ms;
 
-	//printf("Enter the amount of cars: ");
-	//scanf_s("%lf", &y);
+	/*while (k != 30) {
+		printf("\nTest %d:\n", k + 1);*/
+		double y = 0.0;
+		double in_v[MAX_CARS] = { 0.0 };
+		double fin_v[MAX_CARS] = { 0.0 };
+		double time_s[MAX_CARS] = { 0.0 };
 
-	y = MAX_CARS;
+		printf("Enter the amount of cars: ");
+		scanf_s("%lf", &y);
 
-	//printf("Enter the initial velocity, final velocity, and time of each car: \n");
-	for (int i = 0; i < y; i++) {
-		/*scanf_s("%lf, ", &in_v[i]);
-		scanf_s("%lf, ", &fin_v[i]);
-		scanf_s("%lf", &time[i]);*/
+		/*y = MAX_CARS;*/
 
-		in_v[i] = (double)rand() / RAND_MAX * 100.0; // Random initial velocity between 0 and 100 km/h
-		fin_v[i] = (double)rand() / RAND_MAX * 200.0; // Random final velocity between 0 and 200 km/h
-		time_s[i] = (double)rand() / RAND_MAX * 15.0; // Random time between 0 and 15s
-	}
+		//printf("Enter the initial velocity, final velocity, and time of each car: \n");
+		for (int i = 0; i < y; i++) {
+			scanf_s("%lf, ", &in_v[i]);
+			scanf_s("%lf, ", &fin_v[i]);
+			scanf_s("%lf", &time_s[i]);
 
-	for (int i = 0; i < y; i++) {
-		printf("Car %d: initial velocity: %.2lf, final velocity: %.2lf, time: %.2lf\n", i, in_v[i], fin_v[i], time_s[i]);
-	}
+			//in_v[i] = (double)rand() / RAND_MAX * 100.0; // Random initial velocity between 0 and 100 km/h
+			//fin_v[i] = (double)rand() / RAND_MAX * 200.0; // Random final velocity between 0 and 200 km/h
+			//time_s[i] = (double)rand() / RAND_MAX * 15.0; // Random time between 0 and 15s
+		}
 
-	double result[MAX_CARS] = { 0.0 };
+	/*	for (int i = 0; i < y; i++) {
+			printf("Car %d: initial velocity: %.2lf, final velocity: %.2lf, time: %.2lf\n", i, in_v[i], fin_v[i], time_s[i]);
+		}*/
 
-	// --- Timing starts here ---
-	LARGE_INTEGER freq, start, end;
-	QueryPerformanceFrequency(&freq);   // ticks per second
-	QueryPerformanceCounter(&start);
+		double result[MAX_CARS] = { 0.0 };
 
-	for (int i = 0; i < y; i++) {
-		result[i] = calc_accel(in_v[i], fin_v[i], time_s[i]);
-	}
+		// --- Timing starts here ---
+		LARGE_INTEGER freq, start, end;
+		QueryPerformanceFrequency(&freq);   // ticks per second
+		QueryPerformanceCounter(&start);
 
-	QueryPerformanceCounter(&end);
+		for (int i = 0; i < y; i++) {
+			result[i] = calc_accel(in_v[i], fin_v[i], time_s[i]);
+		}
 
-	int accel[MAX_CARS] = { 0 };
+		QueryPerformanceCounter(&end);
 
-	printf("\nThe integer acceleration values (m/s^2) are: \n");
-	for (int i = 0; i < y; i++) {
-		printf("%d:", i + 1);
-		printf("%.2lf, ", result[i]);
+		int accel[MAX_CARS] = { 0 };
 
-		accel[i] = lf_to_int(result[i]);
-		printf("%d\n", accel[i]);
-	}
+		printf("\nThe integer acceleration values (m/s^2) are: \n");
+		for (int i = 0; i < y; i++) {
+			printf("%d:", i + 1);
+			printf("%.2lf, ", result[i]);
 
-	double elapsed_ms = (double)(end.QuadPart - start.QuadPart) * 1000.0 / freq.QuadPart;
-	printf("Time for %d calls to calc_accel: %.6f ms\n", (int)y, elapsed_ms);
+			accel[i] = lf_to_int(result[i]);
+			printf("%d\n", accel[i]);
+		}
+
+		calc_accel_ms = (double)(end.QuadPart - start.QuadPart) * 1000.0 / freq.QuadPart;
+		printf("\nTime taken to calculate acceleration for %d cars: %.6lf ms\n", (int)y, calc_accel_ms);
+
+		//save_time(calc_accel_ms, k);
+		/*k++;
+	}*/
 
 	return 0;
 }
