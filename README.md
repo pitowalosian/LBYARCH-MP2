83f03222a27d74ba5df216cf4e60fca72784f5a7
pitowalosian <sian_obregon@dlsu.edu.ph>
2026-08-07 03:07:46 +0800

    added more to readme and created a c equivalent of calc_accel

--- README.md
+++ README.md
@@ -1,28 +1,30 @@
 # LBYARCH-MP2
 
 ### TODO
-- Time the asm function only for input Y size = {10, 100, 1000, 10000}.  If 10000 is impossible, you may reduce it to the point your machine can support. You may use a random number generator to generate values for the input.
-
-- You must run at least 30 times to get the average execution time. 
-
-- For the data, you may initialize each input with the same or different random value. 
-
-- You will need to check the correctness of your output.  
-
 - Output in GitHub (make sure that I can access your Github):
 
-### Average Execution Time 
- Y | 10 | 100 | 1000 | 10000
---- | --- | --- | --- | ---
-Average Execution Time (ns) | 103.33 | 520.00 | 3663.33 | 23216.67
-Min | 0	| 400 | 2200 | 15300
-Max | 200 | 1100 | 8200 | 49900
+## Performance Analysis
 
-### Short Analysis of the Performance
+### ASM Average Execution Time 
+ Y | Average Execution Time (ns) | Min | Max
+--- | --- | --- | --- | ---
+ 10 | 56.67 | 0 | 300 
+ 100 | 476.67 | 200 | 600 
+ 1000 | 3266.67 | 2200 | 15200
+ 9000 | 19116.67 | 14900 | 73700
+
+As seen from the table above, there is a positive relationship between the number of inputs and the average execution time, since the average execution time also increases when the number of inputs increase. This is understandable and valid due to sheer size of inputs per row. For comparison purposes, I created a `calc_accel_c` C function that is programmed to also calculate the acceleration, just like the `calc_accel` block in ASM. In a `y = 10` input size, the average execution time for the C function is `96.67 ns`, while the ASM code's average execution time is `56.67 ns` only. There is only a `40 ns` difference for this input size, however, we can already hypothesize that a larger input size will result to a much wider gap in the execution times of the two functions. To prove this, I used the largest input size my machine could run, `y = 9000`. The recorded average execution time for ASM is `19116.67ns`, while it is a whopping `40053.33ns` for C. There is a drastic `20936.66ns` contrast between the two. From this, we can conclude that the Assembly language is much more efficient than C.
 
 ## Take a screenshot of the program output with the correctness check.
-![Screenshot_with_correctness)](image.png)
-
-
+For this part, I compared the output of the ASM function `calc_accel` with the output of the C function `calc_accel_c`. I also used an external website [see References] to cross-check these answers to get the accuracy of my program. 
+![Screenshot_with_correctness_1)](/screenshots/car1.png)
+![Screenshot_with_correctness_1)](/screenshots/car2.png)
+![Screenshot_with_correctness_1)](/screenshots/car3.png)
+![Screenshot_with_correctness_1)](/screenshots/car4.png)
+![Screenshot_with_correctness_1)](/screenshots/car5.png)
+Note: The program uses round to nearest rounding method, so it is implied that we also use the same rounding method when cross-checking with the acceleration calculator.
 
 ## Short Videos (5-10mins) showing your source code, compilation, and execution of the C and x86-64 program
+
+### References
+https://www.omnicalculator.com/physics/acceleration
--- x86-to-C interface/x86-to-C interface/clbyarch-mp2.c
+++ x86-to-C interface/x86-to-C interface/clbyarch-mp2.c
@@ -3,26 +3,28 @@
 #include <time.h>
 #include <Windows.h>
 
-#define MAX_CARS 9000
+#define MAX_CARS 10
 #define MAX_TEST 30
 
 extern int lf_to_int(double lf);
 extern double calc_accel(double in_v, double fin_v, double time);
 
-void save_time(int calc_accel_ms[], int test_number) {
-	FILE* file = fopen("9000_time.txt", "a"); // Open in append mode
+static void save_time(int calc_accel_ms[], int test_number) {
+	FILE* file = fopen("test.txt", "w"); // Open in append mode
 
 	if (file == NULL) {
 		printf("Error opening file for writing.\n");
 		return;
 	}
 
-	fprintf(file, "%d\n", calc_accel_ms[test_number]);
+	for (int i = 0; i < test_number; i++) {
+		fprintf(file, "%d\n", calc_accel_ms[i]);
+	}
 
 	fclose(file);
 }
 
-double calc_accel_c(double in_v, double fin_v, double time) {
+static double calc_accel_c(double in_v, double fin_v, double time) {
 	double diff = fin_v - in_v;
 	double accel = diff * 1000.0 * 1 / 3600 / time; // Convert km/h to m/s and divide by time
 
@@ -35,15 +37,15 @@ int main() {
 	int k = 0;
 	int calc_accel_ms[MAX_TEST];
 
-	while (k != 30) {
+	while (k != MAX_TEST) {
 		printf("\nTest %d:\n", k + 1);
 		double y = 0.0;
 		double in_v[MAX_CARS] = { 0.0 };
 		double fin_v[MAX_CARS] = { 0.0 };
 		double time_s[MAX_CARS] = { 0.0 };
 
-		/*printf("Enter the amount of cars: ");
-		scanf_s("%lf", &y);*/
+		//printf("Enter the amount of cars: ");
+		//scanf_s("%lf", &y);
 
 		y = MAX_CARS;
 		int int_y = lf_to_int(y); // Convert to integer
@@ -56,42 +58,75 @@ int main() {
 
 			in_v[i] = (double)rand() / RAND_MAX * 100.0; // Random initial velocity between 0 and 100 km/h
 			fin_v[i] = (double)rand() / RAND_MAX * 200.0; // Random final velocity between 0 and 200 km/h
-			time_s[i] = (double)rand() / RAND_MAX * 15.0; // Random time between 0 and 15s
+		    time_s[i] = (double)rand() / RAND_MAX * 15.0; // Random time between 0 and 15s
 		}
 
+		printf("\n--------------------------Output--------------------------\n");
+
 		/*for (int i = 0; i < y; i++) {
-			printf("Car %d: initial velocity: %.2lf, final velocity: %.2lf, time: %.2lf\n", i, in_v[i], fin_v[i], time_s[i]);
+			printf("Car %d: initial velocity: %.2lf, final velocity: %.2lf, time: %.2lf\n", i + 1, in_v[i], fin_v[i], time_s[i]);
 		}*/
 
 		double result[MAX_CARS] = { 0.0 };
+		double result_c[MAX_CARS] = { 0.0 };
 
 		// --- Timing starts here ---
-		LARGE_INTEGER freq, start, end;
-		QueryPerformanceFrequency(&freq);   // ticks per second
-		QueryPerformanceCounter(&start);
 
 		for (int i = 0; i < int_y; i++) {
 			result[i] = calc_accel(in_v[i], fin_v[i], time_s[i]);
 		}
 
+		LARGE_INTEGER freq, start, end;
+		QueryPerformanceFrequency(&freq);   // ticks per second
+		QueryPerformanceCounter(&start);
+
+		for (int i = 0; i < int_y; i++) {
+
+			result_c[i] = calc_accel_c(in_v[i], fin_v[i], time_s[i]);
+
+		}
+
 		QueryPerformanceCounter(&end);
 
 		int accel[MAX_CARS] = { 0 };
+		int accel_c[MAX_CARS] = { 0 };
 
 		printf("\nThe integer acceleration values (m/s^2) are: \n");
+		printf("\t     ASM\t\t\tC\t\tCorrectness Check:\n");
+		
 		for (int i = 0; i < int_y; i++) {
 			printf("Car %d: ", i + 1);
 
 			accel[i] = lf_to_int(result[i]);
-			printf("%d m/s^2\n", accel[i]);
+			printf("%5d m/s^2", accel[i]);  
+
+			accel_c[i] = lf_to_int(result_c[i]);
+			printf("\t\t%5d m/s^2", accel_c[i]);  
+
+			if (accel[i] == accel_c[i]) {
+				printf("\t\t    Correct\n");
+			}
+			else {
+				printf("\t\t    Incorrect\n");
+			}
 		}
 
 		calc_accel_ms[k] = (end.QuadPart - start.QuadPart) * 1e9 / freq.QuadPart;
 		printf("\nTime taken to calculate acceleration for %d cars: %d ns\n", int_y, calc_accel_ms[k]);
 
-		save_time(calc_accel_ms, k);
 		k++;
 	}
+
+	int sum = 0;
+	for (int i = 0; i <= MAX_TEST - 1; i++) {
+		sum += calc_accel_ms[i];
+	}
+	
+	double avg_time = (double)sum / MAX_TEST;
+
+	save_time(calc_accel_ms, k);
+
+	printf("----------------------------------------------------------\nAverage execution time for %d tests: %.2lf ns\n", k, avg_time);
 
 	return 0;
 }
